82c2581e359f14069848c8621b6a2be988126074
= <=>
2017-05-16 19:43:41 -0700

    Added static files

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = flask.Flask(__name__)
 def main():
     return flask.render_template("index.html")
 
+@app.route("/files/<package>/<file>")
+def get_file(package, file):
+    return flask.send_from_directory("packages", "{}/{}".format(package, file))
+
 @app.route("/<package>")
 def get_package(package):
     return flask.send_from_directory("packages", "{}/spam.toml".format(package))

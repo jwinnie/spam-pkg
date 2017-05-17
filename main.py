385b4767af48fc21a6425b8e306d82059c52a9a2
= <=>
2017-05-16 20:09:46 -0700

    Fixed crazy Heroku bug

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ app = flask.Flask(__name__)
 def main():
     return flask.render_template("index.html")
 
-@app.route("/files/<package>/<file>")
+@app.route("/<package>/<file>")
 def get_file(package, file):
     return flask.send_from_directory("packages", "{}/{}".format(package, file))
-
-@app.route("/<package>")
-def get_package(package):
-    return flask.send_from_directory("packages", "{}/spam.toml".format(package))
